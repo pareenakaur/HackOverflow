00a1e0514f8041fb2c7690723a6176c8c3dfca09
lesfgoh <107750978+lesfgoh@users.noreply.github.com>
2023-09-06 13:55:09 +0800

    Update app.py
    Improved event_research and inputs

--- chatgpt-retrieval-main/app.py
+++ chatgpt-retrieval-main/app.py
@@ -8,6 +8,7 @@ from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain, SequentialChain 
 from langchain.memory import ConversationBufferMemory
 from langchain.utilities import WikipediaAPIWrapper 
+import wikipedia
 
 os.environ['OPENAI_API_KEY'] = apikey
 
@@ -18,15 +19,19 @@ prompt = st.text_input('Plug in your prompt here')
 
 # Prompt templates
 input_template = PromptTemplate(
-    input_variables = ['event', 'portfolio'], 
-    template='Your role is to act as a risk analyst, writing a detailed quantitative risk analysis \
-    for an asset manager about how {event} affects the \
-    risk rating of their portfolio with {portfolio} asset weightages.'
+    input_variables = ['event', 'event_research'], 
+    template='Your role is to act as a financial macro-economics researcher. Write a summary of how {event} could \
+    affect financial markets or any investment classes, keeping your response below 100 words. \
+    For more context on this event, refer to the following research: {event_research}'
 )
 
 script_template = PromptTemplate(
-    input_variables = ['wikipedia_research'], 
-    template='Using the following wikipedia research:{wikipedia_research}, my analysis is:'
+    input_variables = ['input_chain_output', 'portfolio', 'portfolio_research'], 
+    template='Your role is to act as a risk analyst. You are given the following \
+    market information, {input_chain_output}. Write a detailed quantitative risk analysis \
+    for how that affects the risk rating of a stock portfolio, {portfolio}. \
+    Keep your response below 200 words.\
+    Refer to research on the portfolio {portfolio_research} for more context'
 )
 
 
@@ -36,17 +41,18 @@ script_memory = ConversationBufferMemory(input_key='wikipedia_research', memory_
 
 # Llms
 llm = OpenAI(temperature=0.9) 
-input_chain = LLMChain(llm=llm, prompt=input_template, verbose=True, output_key='event')
-output_chain = LLMChain(llm=llm, prompt=script_template, verbose=True, output_key='script')
+input_chain = LLMChain(llm=llm, prompt=input_template, verbose=True, output_key='input_chain_output')
+output_chain = LLMChain(llm=llm, prompt=script_template, verbose=True, output_key='output_chain_output')
 
 wiki = WikipediaAPIWrapper()
 
 # Show stuff to the screen if there's a prompt
 portfolio = 'Apple: 60%, Google: 40%'
 if prompt:
-    event = input_chain.run(event=prompt, portfolio = portfolio)
-    wiki_research = wiki.run(prompt)
-    script_output = output_chain.run(event=event, wikipedia_research=wiki_research)
+    event_research = wiki.run(prompt)
+    portfolio_research = wiki.run(portfolio)
+    event = input_chain.run(event= prompt, event_research= event_research)
+    script_output = output_chain.run(input_chain_output=event, portfolio = portfolio, portfolio_research = portfolio_research)
 
     st.write(event)
     st.write(script_output) 
@@ -57,5 +63,8 @@ if prompt:
     with st.expander('Script History'): 
         st.info(script_memory.buffer)
 
-    with st.expander('Wikipedia Research'): 
-        st.info(wiki_research)
+    with st.expander('Event Research'): 
+        st.info(event_research)
+
+    with st.expander('Portfolio Research'): 
+        st.info(portfolio_research)
